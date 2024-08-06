084d1adc503da0bdd626ffd77b1149ae6a5a67f0
Yanfeng Liu <yanfengliux@gmail.com>
2024-08-06 22:47:19 +0800

    Add lat long

--- main.py
+++ main.py
@@ -3,49 +3,67 @@ import csv
 
 # Read json to a list of address strings.
 data = json.load(open("data.json"))
-pubs = data["results"]
+results = data["results"]
 locations = []
-for pub in pubs:
-    locations += [pub["address"]]
+for result in results:
+    locations.append(
+        {
+            "address": result["address"].lower(),
+            "lat": result["lat"],
+            "long": result["lon"],
+        }
+    )
 
 # Check if the string can be split into two by a comma.
-valid_locs = {}
-invalid_locs = []
+valid_addresses = {}
+invalid_addresses = []
 for loc in locations:
-    parts = loc.split(", ")
+    address = loc["address"]
+    lat = loc["lat"]
+    long = loc["long"]
+    parts = address.split(", ")
     if len(parts) == 2:
         city = parts[0]
         country = parts[1]
+        city_info = {
+            "city": city,
+            "lat": lat,
+            "long": long,
+        }
         # Group by country.
-        if country in valid_locs:
-            valid_locs[country].append(city)
+        if country in valid_addresses:
+            valid_addresses[country].append(city_info)
         else:
-            valid_locs[country] = [city]
+            valid_addresses[country] = [city_info]
     else:
-        invalid_locs.append(loc)
+        invalid_addresses.append(loc)
 
 # Save "city, country" pair to a CSV file
 with open("city_country.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["City", "Country"])
-    for country in valid_locs:
-        for city in valid_locs[country]:
-            writer.writerow([city, country])
+    writer.writerow(["City", "Country", "Lat", "Long"])
+    for country in valid_addresses:
+        for city_info in valid_addresses[country]:
+            writer.writerow(
+                [city_info["city"], country, city_info["lat"], city_info["long"]]
+            )
 
 # Save country to another CSV file.
 with open("country.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Country"])
-    for country in valid_locs:
+    for country in valid_addresses:
         writer.writerow([country])
 
 # Print valid data.
-print("Saved the following (city, country) to CSV:")
-for country in valid_locs:
-    for city in valid_locs[country]:
-        print(f"{city}, {country}")
+print("Saved the following to CSV:")
+for country in valid_addresses:
+    for city_info in valid_addresses[country]:
+        print(
+            f"{city_info['city']}, {country}, {city_info['lat']}, {city_info['long']}"
+        )
 
 # Print excluded data.
 print("Excluded the following data:")
-for item in invalid_locs:
+for item in invalid_addresses:
     print(item)
