8efbc5fcb1744bc43d400aca45346dbecf1fade0
Yanfeng Liu <yanfengliux@gmail.com>
2024-08-06 20:50:43 +0800

    Add comment; fix print bug

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 import csv
 
-
+# Read json to a list of address strings.
 data = json.load(open("data.json"))
 pubs = data["results"]
 locations = []
 for pub in pubs:
     locations += [pub["address"]]
 
+# Check if the string can be split into two by a comma.
 valid_locs = {}
 invalid_locs = []
 for loc in locations:
@@ -15,6 +16,7 @@ for loc in locations:
     if len(parts) == 2:
         city = parts[0]
         country = parts[1]
+        # Group by country.
         if country in valid_locs:
             valid_locs[country].append(city)
         else:
@@ -22,6 +24,7 @@ for loc in locations:
     else:
         invalid_locs.append(loc)
 
+# Save "city, country" pair to a CSV file
 with open("city_country.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["City", "Country"])
@@ -29,16 +32,20 @@ with open("city_country.csv", mode="w", newline="") as file:
         for city in valid_locs[country]:
             writer.writerow([city, country])
 
+# Save country to another CSV file.
 with open("country.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Country"])
     for country in valid_locs:
         writer.writerow([country])
 
+# Print valid data.
 print("Saved the following (city, country) to CSV:")
-for item in valid_locs:
-    print(item)
+for country in valid_locs:
+    for city in valid_locs[country]:
+        print(f"{city}, {country}")
 
+# Print excluded data.
 print("Excluded the following data:")
 for item in invalid_locs:
     print(item)
